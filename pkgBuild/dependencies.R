bcd75be0fffa57099c5105c8c3357e473d293915
rBatt <battrd@gmail.com>
2016-08-19 21:24:05 -0400

    more pkgs

--- pkgBuild/dependencies.R
+++ pkgBuild/dependencies.R
@@ -3,6 +3,7 @@ update_dependencies <- function(){
 	devtools::use_package("RcppRoll", type="Imports")
 	devtools::use_package("zoo", type="Imports")
 	devtools::use_package("stats", type="Imports")
+	devtools::use_package("graphics", type="Suggests")
 }
 
 
